1f6000ff403780f081407ae1c8b130c751ab4d82
lxl <linxuelei@youmeng.com>
2017-09-26 10:45:16 +0800

    3

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,7 +10,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>关于2</p>
+    <p>关于3</p>
 
     <code><?= __FILE__ ?></code>
 </div>
